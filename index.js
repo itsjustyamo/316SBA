e86a23aaf09aec90d7e4b510d60ef5590929055e
Tshiamo Tsoku <ttsoku@icloud.com>
2024-02-04 20:45:54 -0500

    Second commit

--- index.js
+++ index.js
@@ -1,30 +1,18 @@
-document.addEventListener("DOMContentLoaded", function() {
-    console.log("Hello from index.js!");
-});
-
-
-//Menu Data structure
-let menuLinks = [
-    { text: 'about', href: '/about' },
-    { text: 'catalog', href: '/catalog' },
-    { text: 'orders', href: '/orders' },
-    { text: 'account', href: '/account' },
-  ];
-
-  menuLinks = [
-  {text: 'about', href: '/about'},
-  {text: 'catalog', href: '#', subLinks: [
-    {text: 'all', href: '/catalog/all'},
-    {text: 'top selling', href: '/catalog/top'},
-    {text: 'search', href: '/catalog/search'},
-  ]},
-  {text: 'orders', href: '#' , subLinks: [
-    {text: 'new', href: '/orders/new'},
-    {text: 'pending', href: '/orders/pending'},
-    {text: 'history', href: '/orders/history'},
-  ]},
-  {text: 'account', href: '#', subLinks: [
-    {text: 'profile', href: '/account/profile'},
-    {text: 'sign out', href: '/account/signout'},
-  ]},
-];
+// Cache at least one element using selectElementById.
+// Cache at least one element using querySelector or querySelectorAll.
+// Use the parent-child-sibling relationship to navigate between elements at least once (firstChild, lastChild, parentNode, nextElementSibling, etc.)
+// Iterate over a collection of elements to accomplish some task.
+// Create at least one element using createElement.
+// Use appendChild and/or prepend to add new elements to the DOM.
+// Use the DocumentFragment interface or HTML templating with the cloneNode method to create templated content.
+// Modify the HTML or text content of at least one element in response to user interaction using innerHTML, innerText, or textContent.
+// Modify the style and/or CSS classes of an element in response to user interactions using the style or classList properties.
+// Modify at least one attribute of an element in response to user interaction.
+// Register at least two different event listeners and create the associated event handler functions.
+// Use at least two Browser Object Model (BOM) properties or methods.
+// Include at least one form and/or input with HTML attribute validation.
+// Include at least one form and/or input with DOM event-based validation. (This can be the same form or input as the one above, but should include event-based validation in addition to the HTML attribute validation.)
+// Ensure that the program runs without errors (comment out things that do not work, and explain your blockers - you can still receive partial credit).
+// Commit frequently to the git repository.
+// Include a README file that contains a description of your application.
+// Level of effort displayed in creativity, presentation, and user experience.
